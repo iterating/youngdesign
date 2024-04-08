ee738b77a92e9e498bba82b6029393e9ce5def4e
jy <jy>
2024-04-08 06:57:53 -0700

    yml

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>{{ homepage.title }}</h1>
+    <p>{{ homepage.description }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  async asyncData({ $content }) {
+    // Load and parse the YAML file using YAML loader
+    const homepageData = await import('!!yaml-loader!~/static/home.yml')
+    return { homepage: homepageData.default }
+  }
 }
 </script>
+
+<style scoped>
+/* Add your page styles here */
+</style>
